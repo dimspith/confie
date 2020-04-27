5688626b929ab42dd8f122c6196a82d0b0945a92
dimitrissp <risorg@tuta.io>
2020-04-27 18:30:58 +0300

    Fix confie.nim indentation

--- src/confie.nim
+++ src/confie.nim
@@ -4,8 +4,8 @@ import os
 const cmdArgsError*: string = "Error: Could not get command line arguments!"
 
 when isMainModule:
-    when declared(commandLineParams):
-      let arglist = commandLineParams()
-      parseArgs(arglist)
-    else:
-      quit(cmdArgsError)
+  when declared(commandLineParams):
+    let arglist = commandLineParams()
+    parseArgs(arglist)
+  else:
+    quit(cmdArgsError)
