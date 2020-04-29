70bbcfd8ad4510d68360c5b0a94e353a02a0b462
dimitrissp <risorg@tuta.io>
2020-04-30 00:06:02 +0300

    Refactor type declarations

--- src/types.nim
+++ src/types.nim
@@ -1,15 +1,10 @@
 type
-  Packages = object
-    package_manager: string
-    package_list: seq[string]
-
   Dotfile = object
     name: string
-    link: bool
     local_path: string
     install_path: string
-    required_packages: seq[string]
 
-  Config* = object
-    packageConfig: Packages
+  Conf* = object
+    package_list: seq[string]
     dotfiles: seq[Dotfile]
+
