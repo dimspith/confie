117e746abb62abc99618e4262630b2387ccd4da5
dimitrissp <risorg@tuta.io>
2020-04-29 23:36:48 +0300

    Remove incomplete function

--- src/types.nim
+++ src/types.nim
@@ -1,5 +1,3 @@
-import strformat
-
 type
   Packages = object
     package_manager: string
@@ -15,13 +13,3 @@ type
   Config* = object
     packageConfig: Packages
     dotfiles: seq[Dotfile]
-
-proc configString*(cfg: Config): string =
-  return &"""
-Packages:
-  Package Manager: {cfg.package_manager}
-  Package List: {cfg.package_list}
-
-Dotfiles:
-  
-"""
