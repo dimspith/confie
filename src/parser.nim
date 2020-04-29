ae0d9016d0cf4926ffe9484d99e692fdc10f6ad3
DarkPhantom <l2kwstasl2@gmail.com>
2020-04-29 23:22:34 +0300

    minor changes

--- src/parser.nim
+++ src/parser.nim
@@ -1,7 +1,9 @@
 import parsetoml
 
-proc parseTOMLFile*(location: TaintedString): string =
+proc parseTOMLFile*(location: string): string =
   ## Parse the toml config file and fill the required variables
   let tomlTable = parseToml.parseFile(location)
   return tomlTable.toTomlString
 
+when isMainModule:
+  let s = parseTOMLFile("config.toml")
