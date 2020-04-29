73bbf7ea312fe6154957f004d609cf706daf2d94
DarkPhantom <l2kwstasl2@gmail.com>
2020-04-30 01:38:51 +0300

    parse confie.cfg

--- src/parser.nim
+++ src/parser.nim
@@ -1,6 +1,23 @@
-import parsecfg
+import parsecfg, types, tables, sugar
 
 proc parseConfig*(location: string): string =
   ## Parse the toml config file and fill the required variables
-  location
+  try:
+    let cfg = loadConfig(location)
+    var conf = Conf(
+      dotfiles: @[],
+      packages: @[]
+    )
+    for section, key in cfg.pairs:
+      let dotfile = Dotfile(
+        name: section,
+        local_path: key.getOrDefault("local_path"),
+        install_path: key.getOrDefault("install_path")
+      )
+      conf.dotfiles.add(dotfile)
+  except IOError:
+    return "Invalid filename"
 
+when isMainModule:
+  echo parseConfig("confie.cfg")
+  echo parseConfig("qq.cfg")
--- src/types.nim
+++ src/types.nim
@@ -1,12 +1,12 @@
 type
-  Dotfile = object
-    name: string
-    local_path: string
-    install_path: string
+  Dotfile* = object
+    name*: string
+    local_path*: string
+    install_path*: string
 
 type
-  Conf* = object
-    dotfiles: seq[Dotfile]
-    packages: seq[string]
-      
+  Conf* = ref object
+    dotfiles*: seq[Dotfile]
+    packages*: seq[string]
+
 
